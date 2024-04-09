750189543058f6d2e026f856676cb5646dda778b
AthallaITS <5027231018@student.its.ac.id>
2024-04-09 19:36:33 +0700

    Chapter 9: Streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,8 +14,8 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
   noStore();
+
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
